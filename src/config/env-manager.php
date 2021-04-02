aa159e7035c16e03578646887dafd07b84c8ea0d
ncrewsSouthern <ncrews@southerninsurance.net>
2021-04-02 09:48:17 -0500

    update var naming

--- src/config/env-manager.php
+++ src/config/env-manager.php
@@ -12,12 +12,12 @@ return [
     /*
      * Location of ENV source files
      */
-    'source_path' => env( 'SOURCE_ENV_PATH', 'env_files' ),
+    'remote_directory' => env( 'SOURCE_ENV_PATH', 'env_files' ),
 
     /*
      * Local storage path for env files ( default project root/base )
      */
-    'local_path'  => env( 'LOCAL_ENV_PATH', '.' ),
+    'local_directory'  => env( 'LOCAL_ENV_PATH', '.' ),
 
     /*
      * List of files to process when no single file is specified.
